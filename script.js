005e954fe8d8e41acd63c6f01b05d8e2d428a59c
KevFroo <193817275+KevFroo@users.noreply.github.com>
2025-11-15 16:55:04 -0600

    Complete Functions

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log("Halo!");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if(choice === 0) {
+        return "rock";
+    } else if(choice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    return prompt("Please enter your choice (rock, paper, scissors): ");
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    let lowerHumanChoice = humanChoice.toLowerCase();
+
+    if(lowerHumanChoice === 'rock' && computerChoice === 'rock') {
+        console.log("You Tied!");
+    } else if(lowerHumanChoice === 'rock' && computerChoice === 'paper') {
+        console.log("You lose! " + computerChoice + " beats " + lowerHumanChoice);
+        computerChoice++;
+    } else if(lowerHumanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log("You win! " + lowerHumanChoice + " beats " + computerChoice);
+        humanScore++;
+    } else if(lowerHumanChoice === 'paper' && computerChoice === 'rock') {
+        console.log("You win! " + lowerHumanChoice + " beats " + computerChoice);
+        humanScore++;
+    } else if(lowerHumanChoice === 'paper' && computerChoice === 'paper') {
+        console.log("You Tied!");
+    } else if(lowerHumanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log("You lose! " + computerChoice + " beats " + lowerHumanChoice);
+        computerChoice++;
+    } else if(lowerHumanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log("You lose! " + computerChoice + " beats " + lowerHumanChoice);
+        computerChoice++;
+    } else if(lowerHumanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log("You win! " + lowerHumanChoice + " beats " + computerChoice);
+        humanScore++;
+    } else if(lowerHumanChoice === 'scissors' && computerChoice === 'scissors') {
+        console.log("You Tied!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
